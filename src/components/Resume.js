9d26208d5b7bbbfb9fb0f885c3c92f890760c64d
VSudharson <balajisudharson99@gmail.com>
2024-08-07 13:38:49 -0800

    Modified

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,8 +1,9 @@
 import ResumeImg from '../assets/resume.jpg' ;
+import CV from '../assets/Sudharson V.pdf';
 
 export default function Resume () {
     const config = {
-        link : 'https://drive.google.com/file/d/1GtUl69iICRrPucdzc2c9ZAainWgUYZy4/view?usp=drivesdk'
+        link : '../assets/Sudharson V.pdf'
     }
 
     return <section className='flex flex-col md:flex-row bg-secondary px-5' id='resume'>
@@ -12,7 +13,7 @@ export default function Resume () {
         <div className='md:w-1/2 flex justify-center'>
             <div className='flex flex-col justify-center text-white'>
                 <h1 className='text-4xl border-b-4 border-primary mb-5 w-[140px] font-bold'>Resume</h1>
-                <p className='pb-5'>Hi, this is my Resume. Please check it out. <a target='_blank' className='btn' href={config.link}>Download</a></p>
+                <p className='pb-5'>Hi, this is my Resume. Please check it out. <a target='_blank' className='btn' href={CV} download="Sudharson V">Download<i class="fas fa-download"></i></a></p>
             </div>
         </div>
     </section>
